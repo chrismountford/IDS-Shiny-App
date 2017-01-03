904a2cccb34b9623909033c681f53a3e249f4855
CMountford <christopher.mountford@abagri.com>
2017-01-03 15:49:37 +0000

    Added slider input for years onto UI. Also defined variables for these in global

--- global.R
+++ global.R
@@ -2,12 +2,17 @@ library(ggplot2)
 library(reshape2)
 library(shiny)
 
-# Read in data frames
+## Read in data frames
 ids_data <- read.csv("IDS_Data.csv", header = TRUE)
 ids_country <- read.csv("IDS_Country.csv", header = TRUE)
 
-## IDS Data requires melting by year - also need to remove 'X' from these year names
 
+## IDS Data requires melting by year - also need to remove 'X' from these year names
 ids_data_melted <- melt(ids_data, id.vars = 1:4, variable.name = "Year")
 
 ids_data_melted$Year <- as.numeric(gsub("X", "", ids_data_melted$Year))
+
+
+## Define vars for UI
+min_year <- min(ids_data_melted$Year, na.rm = T)
+max_year <- max(ids_data_melted$Year, na.rm = T)
--- ui.R
+++ ui.R
@@ -1,7 +1,11 @@
 shinyUI(
   navbarPage("Temp Name 1",
-              sidebarLayout(sidebarPanel(
-                
+              sidebarLayout(
+                sidebarPanel(
+                sliderInput("years", "Select a Range of Years:",
+                            min = min_year,
+                            max = max_year,
+                            value = c(min_year, max_year))
               ),
               
               mainPanel(
